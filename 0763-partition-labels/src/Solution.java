1e16b5dfbd489f1ed3e4cd9c662fc01ae27cf88a
liuawen <157514367@qq.com>
2020-10-22 21:39:55 +0800

    763-partition-labels

---
+++ 0763-partition-labels/src/Solution.java
@@ -0,0 +1,9 @@
+/**
+ * @author Liu Awen Email:willowawen@gmail.com
+ * @create 2020-10-22
+ */
+public class Solution {
+    //763-partition-labels
+    //字符串 S 由小写字母组成。
+    //我们要把这个字符串划分为尽可能多的片段，同一个字母只会出现在其中的一个片段。返回一个表示每个字符串片段的长度的列表。
+}
